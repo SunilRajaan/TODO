e8b50bfb44d20b06400b565bfdb8a71d8742eca1
SunilRajaan <beingsunilmagar@gmail.com>
2025-07-10 21:59:00 +0545

    CSRF token

--- base/views.py
+++ base/views.py
@@ -5,3 +5,12 @@ from .models import Todo
 def home(request):
     todos = Todo.objects.all()
     return render(request, 'index.html', context={'todos':todos})
+
+def create_todo(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')        
+        description = request.POST.get('description')        
+        status = request.POST.get('status')      
+
+        Todo.objects.create(name=name, description=description, status=status) 
+    return render(request, 'create.html')
